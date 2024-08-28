e244094ba4a8cbe46902250e7605cdec2dd7827b
ANEESHNAIR2020 <aneeshcgnair@gmail.com>
2024-08-28 15:31:44 +0530

    not completed

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import { Home } from "@mui/icons-material";
+import Profile from "./src/component/pages/profile/Profile";
+import Login from "./src/component/login/Login";
+
+function App() {
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
+
+export default App;
